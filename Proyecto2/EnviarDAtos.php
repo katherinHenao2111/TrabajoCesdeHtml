32b36aa02f3aa914922ba111f879bb9bb17f1966
Unknown <Katheriinhenao@gmail.com>
2018-09-19 16:01:57 -0500

    EnviarD

---
+++ Proyecto2/EnviarDAtos.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form action="Registros.php" method="get">
+            <label>Nombre</label>
+            <input type="text" name="Nombre" placeholder="nombre">
+            <br><br>
+             <label>Apellido</label>
+            <input type="text" name="Apellido" placeholder="apellido">
+            <br><br>
+             <label>Telefono</label>
+            <input type="text" name="Telefono" placeholder="telefono">
+            <br><br>
+            <input type="submit" value="enviar"> 
+        </form>
+        <?php
+
+        ?>
+    </body>
+</html>
+
---
+++ Proyecto2/Registros.php
@@ -0,0 +1,12 @@
+<?php
+    //obtener datos por get
+   $usuario = $_GET;
+   var_dump($usuario);
+   
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
