1c38711fa2fa94c065ec8d6ae388ba68e2f5bfd5
Unknown <Katheriinhenao@gmail.com>
2018-09-19 17:10:30 -0500

    2

--- Proyecto2/Registros.php
+++ Proyecto2/Registros.php
@@ -2,7 +2,7 @@
     //obtener datos por get
    $usuario = $_GET;
    var_dump($usuario);
-   
+   //echo ($usuario['nombre']); Mostrar solo un campo
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
