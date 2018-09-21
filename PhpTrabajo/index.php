da6fb4797967cf557f67ceb3f225f5fb0778ba66
Unknown <Katheriinhenao@gmail.com>
2018-09-21 14:11:16 -0500

    Bootstrap.js

---
+++ PhpTrabajo/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.    
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title></title>
+    </head>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/EstilosIndex.css"/>
+   
+    <body>
+        <header>
+            <!-- Image and text -->
+            <nav class="navbar navbar-light barra-nav">
+              <a class="navbar-brand" href="#">
+                  <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+                Bootstrap
+              </a>
+            </nav>
+        </header>
+        <?php
+        // put your code here
+        ?>
+        
+        
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
